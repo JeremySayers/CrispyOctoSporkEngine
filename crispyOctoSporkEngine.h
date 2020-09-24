5a6d185e140b09622d0733962b64f279f8cc1712
Jeremy Sayers <jeremy1080@gmail.com>
2020-09-24 14:52:11 -0400

    Added a basic Entity class that will by defualt render its texture

--- crispyOctoSporkEngine.h
+++ crispyOctoSporkEngine.h
@@ -64,7 +64,7 @@ namespace CrispyOctoSpork
 		/// <returns>Returns a boolean.</returns>
 		virtual bool OnDestroy();
 
-	private:
+	protected:
 		SDL_Window* window;
 		SDL_Renderer* renderer;
 		int screenWidth;
@@ -80,18 +80,52 @@ namespace CrispyOctoSpork
 		static void Update(void* arg);
 	};
 
+	class Texture
+	{
+	public:
+		Texture();
+		Texture(SDL_Renderer* renderer);
+		~Texture();
+		bool LoadTextureFromFile(SDL_Renderer* renderer, const char* filepath);
+		void Free();
+		void Render(double x, double y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_FPoint* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+		SDL_Renderer* GetRenderer();
+	private:
+		SDL_Texture* texture;
+		SDL_Renderer* renderer;
+		int width;
+		int height;
+	};
+
+	class Entity
+	{
+	public:
+		Entity();
+		Entity(float width, float height, float x, float y, Texture* texture = NULL);
+		~Entity();
+		virtual bool OnUpdate(float deltaTime);
+		virtual bool OnRender(float deltaTime);
+		SDL_Rect GetRect();
+	private:
+		float width;
+		float height;
+		float x;
+		float y;
+		Texture* texture;
+	};
+
 	Engine::Engine()
 	{
 		SDL_Init(SDL_INIT_VIDEO);
 
 		window = NULL;
 		renderer = NULL;
-		int screenWidth = 0;
-		int screenHeight = 0;
-		bool isVsyncEnabled = true;
-		bool isFullscreenEnabled = false;
-		std::string name = "";
-		bool isEngineRunning = false;
+		screenWidth = 0;
+		screenHeight = 0;
+		isVsyncEnabled = true;
+		isFullscreenEnabled = false;
+		name = "";
+		isEngineRunning = false;
 	}
 
 	Engine::~Engine()
@@ -167,6 +201,8 @@ namespace CrispyOctoSpork
 	{
 		isEngineRunning = true;
 
+		this->OnCreate();
+
 		#ifdef __EMSCRIPTEN__
 		emscripten_set_main_loop_arg(Engine::Update, this, -1, 1);
 		#else
@@ -179,6 +215,10 @@ namespace CrispyOctoSpork
 		return true;
 	}
 
+	/// <summary>
+	/// The main loop. To support emscripten the current <see cref="Engine"/> instance is passed in.
+	/// </summary>
+	/// <param name="arg">A pointer to the current <see cref="Engine"/> instance.</param>
 	void Engine::Update(void* arg)
 	{
 		Engine* engine = (Engine*)arg;
@@ -192,10 +232,12 @@ namespace CrispyOctoSpork
 				engine->isEngineRunning = false;
 				break;
 			}
-		}
+		}		
 
 		SDL_SetRenderDrawColor(engine->renderer, 135, 206, 235, 255);
 		SDL_RenderClear(engine->renderer);
+
+		engine->OnUpdate(1.0);
 
 		SDL_RenderPresent(engine->renderer);
 	}
@@ -213,5 +255,105 @@ namespace CrispyOctoSpork
 	bool Engine::OnDestroy()
 	{
 		return true;
+	}
+
+	Entity::Entity()
+	{}
+
+	Entity::Entity(float width, float height, float x, float y, Texture* texture)
+	{
+		this->width = width;
+		this->height = height;
+		this->x = x;
+		this->y = y;
+		this->texture = texture;
+	}
+
+	Entity::~Entity()
+	{
+	}
+
+	 bool Entity::OnUpdate(float deltaTime)
+	{
+		return true;
+	}
+
+	bool Entity::OnRender(float deltaTime)
+	{
+		if (texture != NULL)
+		{
+			texture->Render(x, y);
+		}
+
+		return true;
+	}
+
+	SDL_Rect Entity::GetRect()
+	{
+		SDL_Rect rect = { x, y, width, height };
+		return rect;
+	}
+
+	Texture::Texture()
+	{
+		this->texture = NULL;
+		this->renderer = NULL;
+	}
+
+	Texture::Texture(SDL_Renderer* renderer)
+	{
+		this->texture = NULL;
+		this->width = 0;
+		this->height = 0;
+		this->renderer = renderer;
+	}
+
+	Texture::~Texture()
+	{
+		Free();
+	}
+
+	bool Texture::LoadTextureFromFile(SDL_Renderer* renderer, const char* filepath)
+	{
+		Free();
+
+		texture = IMG_LoadTexture(renderer, filepath);
+
+		if (texture == NULL)
+		{
+			std::cout << "Could not load the texture from: " << filepath << " Error:" << SDL_GetError() << std::endl;
+		}
+
+		SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+
+		return true;
+	}
+
+	void Texture::Free()
+	{
+		if (texture != NULL)
+		{
+			SDL_DestroyTexture(texture);
+			texture = NULL;
+			width = 0;
+			height = 0;
+		}
+	}
+
+	void Texture::Render(double x, double y, SDL_Rect* clip, double angle, SDL_FPoint* center, SDL_RendererFlip flip)
+	{
+		SDL_FRect renderQuad = { x, y, width, height };
+
+		if (clip != NULL)
+		{
+			renderQuad.w = clip->w;
+			renderQuad.h = clip->h;
+		}
+
+		SDL_RenderCopyExF(renderer, texture, clip, &renderQuad, angle, center, flip);
+	}
+	SDL_Renderer* Texture::GetRenderer()
+	{
+		return renderer;
 	}
 }
--- sampleGame.cpp
+++ sampleGame.cpp
@@ -1,20 +1,33 @@
 #include "crispyOctoSporkEngine.h"
+using namespace CrispyOctoSpork;
 
-class SampleGame : public CrispyOctoSpork::Engine
+class Player : public Entity
 {
 public:
-	SampleGame()
-	{}
+	Player(float width, float height, float x, float y, Texture* texture) : Entity(width, height, x, y, texture) {}
+};
 
+class SampleGame : public Engine
+{
+public:
 	bool OnCreate() override
 	{
+		playerTexture = Texture(renderer);
+		playerTexture.LoadTextureFromFile(renderer, "assets/ball.png");
+		player =  Player(32, 32, screenWidth / 2 - 32, screenHeight / 2 - 32, &playerTexture);
 		return true;
 	}
 
 	bool OnUpdate(float deltaTime) override
 	{
+		player.OnUpdate(deltaTime);
+		player.OnRender(deltaTime);
+
 		return true;
 	}
+private:
+	Entity player;
+	Texture playerTexture;
 };
 
 int main(int argc, char* argv[])
